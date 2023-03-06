21cd7fd4e296f17398905a04cb81a7cfca0afe5d
Milton <gmraivan@gmail.com>
2023-03-06 16:47:36 -0500

    Se añadio para leer formato json

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path')
 const port = 4250
 app.use(cors())
 app.use(express.static(__dirname + '/public'))
-
+app.use(express.json())
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, './public/index.html'))
 })
